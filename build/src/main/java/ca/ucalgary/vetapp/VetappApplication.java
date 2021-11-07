d5a430e5b684f4e78c313d0b7fe5d3999dcc4710
Bhavyai Gupta <zbhavyai@gmail.com>
2021-11-07 01:13:23 -0600

    chore: [U888-63] indentation to spaces

--- build/src/main/java/ca/ucalgary/vetapp/VetappApplication.java
+++ build/src/main/java/ca/ucalgary/vetapp/VetappApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VetappApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(VetappApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(VetappApplication.class, args);
+    }
 }

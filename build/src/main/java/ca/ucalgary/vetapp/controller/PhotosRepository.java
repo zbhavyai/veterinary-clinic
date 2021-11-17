eb0e1957f956b9b311a86a4e2d43476b84667dc4
Bhavyai Gupta <zbhavyai@gmail.com>
2021-11-16 23:23:50 -0700

    build: [U888-58, U888-59, U888-60] adding spring repository classes

---
+++ build/src/main/java/ca/ucalgary/vetapp/controller/PhotosRepository.java
@@ -0,0 +1,10 @@
+package ca.ucalgary.vetapp.controller;
+
+import ca.ucalgary.vetapp.model.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PhotosRepository extends JpaRepository<Photos, Long> {
+
+}

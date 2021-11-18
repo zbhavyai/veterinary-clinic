40ee25454d8416ecb3fc02701b9fdea748910f2e
mlee2021 <83717300+mlee2021@users.noreply.github.com>
2021-11-17 21:11:30 -0700

    build: [U888-58, U888-59, U888-60] draft UserController with put, delete and post added

--- build/src/main/java/ca/ucalgary/vetapp/controller/UserController.java
+++ build/src/main/java/ca/ucalgary/vetapp/controller/UserController.java
@@ -53,5 +53,53 @@ public class UserController {
             throw new NotFoundException(id);
         }
     }
+    
+    /**
+     * Saves one user
+     *
+     * @param a
+     * @return
+     */
+    @PostMapping
+    public User addUser(@RequestBody User a) {
+        return this.userRepository.save(a);
+    }
+    
+    /**
+     * Deletes one user
+     *
+     * @param id
+     */
+    @DeleteMapping(path = "{userId}")
+    public void deleteUser(@PathVariable("userId") Long id) {
+        if (!this.userRepository.existsById(id)) {
+            throw new NotFoundException(id);
+        }
+
+        this.userRepository.deleteById(id);
+    }
+    
+    @PutMapping(path = "{userId}")
+    public User updateUser(@RequestBody User a, @PathVariable("userId") Long id) {
+        Optional<User> userOptional = this.userRepository.findById(id);
+
+        if (userOptional.isPresent()) {
+        	User user = userOptional.get();
+        	user.setFirstName(a.getFirstName());
+        	user.setLastName(user.getLastName());
+        	user.setMiddleName(user.getMiddleName());
+        	user.setRole(user.getRole());
+
+            
+
+            return this.userRepository.save(user);
+        }
+
+        else {
+            a.setUserId(id);
+            return this.userRepository.save(a);
+        }
+        
+    }
 
 }

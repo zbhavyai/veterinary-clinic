be5ad178c14e125eac280ca2827fd0df2618fa29
Bhavyai Gupta <zbhavyai@gmail.com>
2021-11-18 23:02:11 -0700

    build: [U888-62] changing user names to distinguish from owners

--- sql/04_dummy_data.sql
+++ sql/04_dummy_data.sql
@@ -1,3 +1,7 @@
+select * from users;
+
+
+-- owners ---------------------------------------
 INSERT INTO owners (o_ownerid,o_firstname,o_middlename,o_lastname,o_role,o_contactnumber,o_emailid,o_address) VALUES
 (1,"Damian","Bruce","Wayne",NULL,"4036653061","damian.bruce.wayne@ucalgary.ca","Wayne Manor"),
 (2,"Yorrick",NULL,"Brown",NULL,"4034097804","yorrick.brown@ucalgary.ca",NULL),
@@ -7,9 +11,10 @@ INSERT INTO owners (o_ownerid,o_firstname,o_middlename,o_lastname,o_role,o_conta
 (6,"James",NULL,"Gunn",NULL,"4035644337","james.gunn@ucalgary.ca","9336 Civic Center Drive, Beverly Hills, CA 90210-3604, USA");
 
 UPDATE sequence_owners SET next_val=7;
+-- ----------------------------------------------
 
 
-
+-- animals --------------------------------------
 INSERT INTO animals (a_animalid,a_name,a_type,a_breed,a_birthdate,a_sex,a_status,a_ownerid,a_tattoonum,a_rfidnumber,a_microchipnumber,a_weight,a_coatcolor,a_continuousmedication) VALUES
 (1,"Ace","Dog","German shepherd",NULL,"1",3,1,130110477,"229664665","229348845",NULL,"Black",NULL),
 (2,"Ampersand","Monkey","Capuchin monkey",NULL,"0",1,2,204261377,"313829102","233453231",NULL,"Brown",NULL),
@@ -20,18 +25,24 @@ INSERT INTO animals (a_animalid,a_name,a_type,a_breed,a_birthdate,a_sex,a_status
 (7,"Streaky","Cat","Abyssinian",NULL,"1",1,3,209577225,"177988155","267795413",NULL,"Orange",NULL);
 
 UPDATE sequence_animals SET next_val=8;
+-- ----------------------------------------------
 
-INSERT INTO users (u_userid,u_firstname,u_middlename,u_lastname,u_role) VALUES
-(1,"Damian","Bruce","Wayne","1"),
-(2,"Tony",NULL,"Stark","2"),
-(3,"Clark","Joseph","Kent","3"),
-(4,"Billy ","Ray","Cyrus","2"),
-(5,"Bob","Homer","Simposon","2"),
-(6,"Miley","Ray",NULL,"1"),
-(7,"Chris",NULL,"Lighter","2");
+
+-- users --------------------------------------
+INSERT INTO users (u_userid,u_firstname,u_middlename,u_lastname,u_role,u_emailid,u_password,u_status) VALUES
+(1,"Greg",NULL,"Boorman",0,"greg.boorman@ucalgary.ca","passw0rd",1),
+(2,"Teacher",NULL,"Admin",0,"teacher.admin@ucalgary.ca","passw0rd",0),
+(3,"Technician",NULL,"A",2,"technician.a@ucalgary.ca","passw0rd",0),
+(4,"Attendant",NULL,"B",1,"attendant.b@ucalgary.ca","passw0rd",0),
+(5,"Teacher",NULL,"C",3,"teacher.c@ucalgary.ca","passw0rd",0),
+(6,"Student",NULL,"D",4,"student.d@ucalgary.ca","passw0rd",1),
+(7,"Student",NULL,"E",4,"student.e@ucalgary.ca","passw0rd",1);
 
 UPDATE sequence_users SET next_val=8;
+-- ----------------------------------------------
 
+
+-- treatments ------------------------------------
 INSERT INTO treatments (t_treatmentid,t_treatmentdesc,t_animalid,t_treatmentdate,t_treatedby) VALUES
 (1,"Physical exam","7","2021-12-08","1"),
 (2,"Blood work","1","2021-12-09","2"),
@@ -42,7 +53,10 @@ INSERT INTO treatments (t_treatmentid,t_treatmentdesc,t_animalid,t_treatmentdate
 (7,"Revolution treatment","7","2021-12-08","4");
 
 UPDATE sequence_treatments SET next_val=8;
+-- ----------------------------------------------
 
+
+-- issues ---------------------------------------
 INSERT INTO issues (i_issueid,i_issuedesc,i_animalid,i_detecteddate,i_raisedby,i_isresolved) VALUES
 (1,"Limp Walk","3","2021-12-08","1",1),
 (2,NULL,"6","2020-12-12","1",0),
@@ -53,7 +67,10 @@ INSERT INTO issues (i_issueid,i_issuedesc,i_animalid,i_detecteddate,i_raisedby,i
 (7,"Upset Stomach","3","2021-11-17","2",NULL);
 
 UPDATE sequence_issues SET next_val=8;
+-- ----------------------------------------------
 
+
+-- photos ---------------------------------------
 INSERT INTO photos (p_photoid,p_animalid,p_photolink) VALUES
 (1,"7","image1.png"),
 (2,"2","image2.png"),
@@ -64,9 +81,10 @@ INSERT INTO photos (p_photoid,p_animalid,p_photolink) VALUES
 (7,"6","image5.png");
 
 UPDATE sequence_photos SET next_val=8;
+-- ----------------------------------------------
 
 
-
+-- comments -------------------------------------
 INSERT INTO comments (c_commentid,c_commentdesc,c_animalid,c_commentdate,c_commenter) VALUES
 (1,"Nighttime terror","2","2021-12-08","1"),
 (2,"Howling ","2","2021-12-09","4"),
@@ -77,5 +95,4 @@ INSERT INTO comments (c_commentid,c_commentdesc,c_animalid,c_commentdate,c_comme
 (7,"Not sleeping","2","2021-12-08","1");
 
 UPDATE sequence_comments SET next_val=8;
-
-
+-- ----------------------------------------------

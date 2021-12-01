2fddf06e616b9551fead62226b1db40fd4a09408
Bhavyai Gupta <zbhavyai@gmail.com>
2021-12-01 16:02:07 -0700

    build: [U888-62] created views and more dummy data

--- sql/03_views.sql
+++ sql/03_views.sql
@@ -1,5 +1,257 @@
+USE vetdb;
+
+-- animals basic view ---------------------------
+CREATE OR REPLACE VIEW animals_basic_view AS
+SELECT
+a.a_animalid 'ID',
+a.a_name 'NAME',
+a.a_species 'SPECIES',
+a.a_subspecies 'SUBSPECIES',
+a.a_breed 'BREED',
+a.a_type 'TYPE',
+a.a_region 'REGION',
+a.a_distinctfeature 'DISTINCT_FEATURE',
+CASE a.a_status
+    WHEN 0 THEN 'GREEN'
+    WHEN 1 THEN 'YELLOW'
+    WHEN 2 THEN 'RED'
+    WHEN 3 THEN 'INACTIVE'
+END 'STATUS',
+CONCAT( (CASE WHEN o.o_firstname IS NULL THEN "" ELSE o.o_firstname END),
+        (CASE WHEN o.o_middlename IS NULL THEN "" ELSE CONCAT(" ", o.o_middlename ) END),
+        (CASE WHEN o.o_lastname IS NULL THEN "" ELSE CONCAT(" ", o.o_lastname ) END)) 'OWNER',
+o.o_address 'LOCATION'
+FROM
+animals a
+LEFT JOIN owners o ON o.o_ownerid = a.a_ownerid
+ORDER BY
+a.a_animalid;
+-- ----------------------------------------------
+
+
+-- animals profile view -------------------------
+CREATE OR REPLACE VIEW animals_profile_view AS
+SELECT
+a.a_animalid 'ID',
+a.a_name 'NAME',
+a.a_species 'SPECIES',
+a.a_subspecies 'SUBSPECIES',
+a.a_breed 'BREED',
+a.a_type 'TYPE',
+a.a_region 'REGION',
+a.a_sex 'SEX',
+a.a_birthdate 'BIRTH_DATE',
+CASE a.a_status
+    WHEN 0 THEN 'GREEN'
+    WHEN 1 THEN 'YELLOW'
+    WHEN 2 THEN 'RED'
+    WHEN 3 THEN 'INACTIVE'
+END 'STATUS',
+a.a_profilepic 'PROFILE_PIC',
+CONCAT( (CASE WHEN o.o_firstname IS NULL THEN "" ELSE o.o_firstname END),
+        (CASE WHEN o.o_middlename IS NULL THEN "" ELSE CONCAT(" ", o.o_middlename ) END),
+        (CASE WHEN o.o_lastname IS NULL THEN "" ELSE CONCAT(" ", o.o_lastname ) END)) 'OWNER',
+o.o_emailid 'EMAIL',
+o.o_contactnumber 'CONTACT',
+o.o_address 'LOCATION',
+a.a_tattoonum 'TATTOO',
+a.a_citytattoo 'CITY_TATTOO',
+a.a_rfidnumber 'RFID',
+a.a_microchipnumber 'MICROCHIP',
+a.a_coatcolor 'COAT_COLOR',
+a.a_continuousmedication 'MEDICATION',
+a.a_distinctfeature 'DISTINCT_FEATURE'
+FROM
+animals a
+LEFT JOIN owners o ON o.o_ownerid = a.a_ownerid
+ORDER BY
+a.a_animalid, o.o_ownerid;
+-- ----------------------------------------------
+
+
+-- animals weights view -------------------------
+CREATE OR REPLACE VIEW animals_weights_view AS
+SELECT
+a.a_animalid 'ID',
+a.a_name 'NAME',
+a.a_species 'SPECIES',
+a.a_subspecies 'SUBSPECIES',
+a.a_breed 'BREED',
+-- a.a_type 'TYPE',
+-- a.a_region 'REGION',
+a.a_distinctfeature 'DISTINCT_FEATURE',
+CASE a.a_status
+    WHEN 0 THEN 'GREEN'
+    WHEN 1 THEN 'YELLOW'
+    WHEN 2 THEN 'RED'
+    WHEN 3 THEN 'INACTIVE'
+END 'STATUS',
+w.w_massinkg 'WEIGHT_KG',
+w.w_recorddate 'RECORD_DATE',
+CONCAT( (CASE WHEN u.u_firstname IS NULL THEN "" ELSE u.u_firstname END),
+        (CASE WHEN u.u_middlename IS NULL THEN "" ELSE CONCAT(" ", u.u_middlename ) END),
+        (CASE WHEN u.u_lastname IS NULL THEN "" ELSE CONCAT(" ", u.u_lastname ) END)) 'RECORDER_NAME',
+u.u_emailid 'RECORDER_EMAIL'
+FROM
+animals a
+LEFT JOIN weights w ON w.w_animalid = a.a_animalid
+LEFT JOIN users u ON u.u_userid = w.w_recordedby
+ORDER BY
+a.a_animalid, w.w_recorddate DESC;
+-- ----------------------------------------------
+
+
+-- animals photos view --------------------------
+CREATE OR REPLACE VIEW animals_photos_view AS
+SELECT
+a.a_animalid 'ID',
+a.a_name 'NAME',
+a.a_species 'SPECIES',
+a.a_subspecies 'SUBSPECIES',
+a.a_breed 'BREED',
+-- a.a_type 'TYPE',
+-- a.a_region 'REGION',
+a.a_distinctfeature 'DISTINCT_FEATURE',
+CASE a.a_status
+    WHEN 0 THEN 'GREEN'
+    WHEN 1 THEN 'YELLOW'
+    WHEN 2 THEN 'RED'
+    WHEN 3 THEN 'INACTIVE'
+END 'STATUS',
+p.p_photolink 'PHOTO_LINK',
+p.p_alttext 'ALT_TEXT',
+p.p_photodesc 'PHOTO_DESC',
+p.p_uploaddate 'UPLOAD_DATE',
+CONCAT( (CASE WHEN u.u_firstname IS NULL THEN "" ELSE u.u_firstname END),
+        (CASE WHEN u.u_middlename IS NULL THEN "" ELSE CONCAT(" ", u.u_middlename ) END),
+        (CASE WHEN u.u_lastname IS NULL THEN "" ELSE CONCAT(" ", u.u_lastname ) END)) 'UPLOADER_NAME',
+u.u_emailid 'UPLOADER_EMAIL'
+FROM
+animals a
+LEFT JOIN photos p ON p.p_animalid = a.a_animalid
+LEFT JOIN users u ON u.u_userid = p.p_uploader
+ORDER BY
+a.a_animalid, p.p_uploaddate DESC;
+-- ----------------------------------------------
+
+
+-- animals comments view ------------------------
+CREATE OR REPLACE VIEW animals_comments_view AS
+SELECT
+a.a_animalid 'ID',
+a.a_name 'NAME',
+a.a_species 'SPECIES',
+a.a_subspecies 'SUBSPECIES',
+a.a_breed 'BREED',
+-- a.a_type 'TYPE',
+-- a.a_region 'REGION',
+a.a_distinctfeature 'DISTINCT_FEATURE',
+CASE a.a_status
+    WHEN 0 THEN 'GREEN'
+    WHEN 1 THEN 'YELLOW'
+    WHEN 2 THEN 'RED'
+    WHEN 3 THEN 'INACTIVE'
+END 'STATUS',
+c.c_commentdesc 'COMMENT',
+c.c_commentdate 'COMMENT_DATE',
+CONCAT( (CASE WHEN u.u_firstname IS NULL THEN "" ELSE u.u_firstname END),
+        (CASE WHEN u.u_middlename IS NULL THEN "" ELSE CONCAT(" ", u.u_middlename ) END),
+        (CASE WHEN u.u_lastname IS NULL THEN "" ELSE CONCAT(" ", u.u_lastname ) END)) 'COMMENTER_NAME',
+u.u_emailid 'COMMENTER_EMAIL'
+FROM
+animals a
+LEFT JOIN comments c ON c.c_animalid = a.a_animalid
+LEFT JOIN users u ON u.u_userid = c.c_commenter
+ORDER BY
+a.a_animalid, c.c_commentdate DESC;
+-- ----------------------------------------------
+
+
+-- animals issues view --------------------------
+CREATE OR REPLACE VIEW animals_issues_view AS
+SELECT
+a.a_animalid 'ID',
+a.a_name 'NAME',
+a.a_species 'SPECIES',
+a.a_subspecies 'SUBSPECIES',
+a.a_breed 'BREED',
+-- a.a_type 'TYPE',
+-- a.a_region 'REGION',
+a.a_distinctfeature 'DISTINCT_FEATURE',
+CASE a.a_status
+    WHEN 0 THEN 'GREEN'
+    WHEN 1 THEN 'YELLOW'
+    WHEN 2 THEN 'RED'
+    WHEN 3 THEN 'INACTIVE'
+END 'STATUS',
+i.i_issuedesc 'ISSUE',
+i.i_detecteddate 'DETECTION_DATE',
+CONCAT( (CASE WHEN u.u_firstname IS NULL THEN "" ELSE u.u_firstname END),
+        (CASE WHEN u.u_middlename IS NULL THEN "" ELSE CONCAT(" ", u.u_middlename ) END),
+        (CASE WHEN u.u_lastname IS NULL THEN "" ELSE CONCAT(" ", u.u_lastname ) END)) 'RAISEDBY_NAME',
+u.u_emailid 'RAISEDBY_EMAIL'
+FROM
+animals a
+LEFT JOIN issues i ON i.i_animalid = a.a_animalid
+LEFT JOIN users u ON u.u_userid = i.i_raisedby
+ORDER BY
+a.a_animalid, i.i_detecteddate DESC;
+-- ----------------------------------------------
+
+
+-- animals treatments view ----------------------
+CREATE OR REPLACE VIEW animals_treatments_view AS
+SELECT
+a.a_animalid 'ID',
+a.a_name 'NAME',
+a.a_species 'SPECIES',
+a.a_subspecies 'SUBSPECIES',
+a.a_breed 'BREED',
+-- a.a_type 'TYPE',
+-- a.a_region 'REGION',
+a.a_distinctfeature 'DISTINCT_FEATURE',
+CASE a.a_status
+    WHEN 0 THEN 'GREEN'
+    WHEN 1 THEN 'YELLOW'
+    WHEN 2 THEN 'RED'
+    WHEN 3 THEN 'INACTIVE'
+END 'STATUS',
+t.t_treatmentdesc 'TREATMENT',
+t.t_drugname 'DRUG_USED',
+t.t_drugdose 'DRUG_DOSE',
+t.t_deliverymethod 'DRUG_DELIVERY',
+t.t_treatmentdate 'TREATMENT_DATE',
+CONCAT( (CASE WHEN u.u_firstname IS NULL THEN "" ELSE u.u_firstname END),
+        (CASE WHEN u.u_middlename IS NULL THEN "" ELSE CONCAT(" ", u.u_middlename ) END),
+        (CASE WHEN u.u_lastname IS NULL THEN "" ELSE CONCAT(" ", u.u_lastname ) END)) 'TREATEDBY_NAME',
+u.u_emailid 'TREATEDBY_EMAIL'
+FROM
+animals a
+LEFT JOIN treatments t ON t.t_animalid = a.a_animalid
+LEFT JOIN users u ON u.u_userid = t.t_treatedby
+ORDER BY
+a.a_animalid, t.t_treatmentdate DESC;
+-- ----------------------------------------------
+
+
+-- owners view ----------------------------------
+CREATE OR REPLACE VIEW owners_view AS
+SELECT
+o.o_ownerid 'ID',
+o.o_firstname 'FIRST_NAME',
+o.o_middlename 'MID_NAME',
+o.o_lastname 'LAST_NAME',
+o.o_emailid 'EMAIL',
+o.o_contactnumber 'PHONE_NUMBER',
+o.o_address 'LOCATION'
+FROM
+owners o
+ORDER BY o.o_ownerid;
+-- ----------------------------------------------
+
+
 -- users view -----------------------------------
-CREATE OR REPLACE VIEW VIEW_USERS AS
+CREATE OR REPLACE VIEW users_view AS
 SELECT
 u.u_userid 'ID',
 u.u_firstname 'FIRST_NAME',
@@ -16,58 +268,11 @@ END 'ROLE',
 CASE u.u_status
     WHEN 0 THEN 'INACTIVE'
     WHEN 1 THEN 'ACTIVE'
-END 'STATUS'
-FROM
-users u;
--- ----------------------------------------------
-
-
--- animals view ---------------------------------
-CREATE OR REPLACE VIEW VIEW_ANIMALS AS
-SELECT
-a.a_animalid 'ID',
-a.a_name 'NAME',
-a.a_type 'TYPE',
-a.a_breed 'BREED',
-a.a_birthdate 'BIRTH_DATE', -- to be replaced with age in months
-a.a_sex 'SEX',
-CASE a.a_status
-    WHEN 0 THEN 'GREEN'
-    WHEN 1 THEN 'YELLOW'
-    WHEN 2 THEN 'RED'
-    WHEN 3 THEN 'INACTIVE'
 END 'STATUS',
-o.o_lastname 'OWNER',
-o.o_emailid 'EMAIL',
-o.o_contactnumber 'CONTACT',
-o.o_address 'ADDRESS',
-a.a_tattoonum 'TATTOO',
-a.a_rfidnumber 'RFID',
-a.a_microchipnumber 'MICROCHIP',
-a.a_coatcolor 'COAT_COLOR',
-a.a_continuousmedication 'MEDICATION'
+u.u_joiningdate 'ACC_CREATION_DATE',
+u.u_activationdate 'ACC_ACTIVATION_DATE',
+u.u_terminationdate 'ACC_TERMINATION_DATE'
 FROM
-animals a
-LEFT JOIN owners o ON a.a_ownerid = o.o_ownerid;
--- ----------------------------------------------
-
-
--- animals view simple --------------------------
-CREATE OR REPLACE VIEW VIEW_ANIMALS_SIMPLE AS
-SELECT
-a.a_animalid 'ID',
-a.a_name 'NAME',
-a.a_type 'TYPE',
-a.a_breed 'BREED',
-CASE a.a_status
-    WHEN 0 THEN 'GREEN'
-    WHEN 1 THEN 'YELLOW'
-    WHEN 2 THEN 'RED'
-    WHEN 3 THEN 'INACTIVE'
-END 'STATUS',
-o.o_firstname 'OWNER'
--- CONCAT(o.o_firstname, " ", (CASE o.o_middlename WHEN IS NULL THEN " " ELSE o.o_middlename END ), " ", o.o_lastname) 'OWNER'
-FROM
-animals a
-LEFT JOIN owners o ON a.a_ownerid = o.o_ownerid;
+users u
+ORDER BY u.u_userid;
 -- ----------------------------------------------
--- sql/04_dummy_data.sql
+++ sql/04_dummy_data.sql
@@ -1,81 +1,88 @@
 -- owners ---------------------------------------
-INSERT INTO owners (o_ownerid,o_firstname,o_middlename,o_lastname,o_role,o_contactnumber,o_emailid,o_address) VALUES
-(1,"Damian","Bruce","Wayne",NULL,"4036653061","damian.bruce.wayne@ucalgary.ca","Wayne Manor"),
-(2,"Yorrick",NULL,"Brown",NULL,"4034097804","yorrick.brown@ucalgary.ca",NULL),
-(3,"Clark","Joseph","Kent",NULL,"4032991533","clark.joseph.kent@ucalgary.ca","Smallville"),
-(4,"Tintin",NULL,NULL,NULL,"4031678053","tintin@ucalgary.ca","Marlinspike Hall, Belgium"),
-(5,"UCalgary",NULL,NULL,0,"4038988801","ucalgary@ucalgary.ca","2500 University Dr NW, Calgary, AB T2N 1N4"),
-(6,"James",NULL,"Gunn",NULL,"4035644337","james.gunn@ucalgary.ca","9336 Civic Center Drive, Beverly Hills, CA 90210-3604, USA");
+INSERT INTO owners (o_ownerid,o_firstname,o_middlename,o_lastname,o_contactnumber,o_emailid,o_address) VALUES
+("1","Damian","Bruce","Wayne","4035624970","damian.bruce.wayne@ucalgary.ca","Wayne Manor"),
+("2","Yorrick",NULL,"Brown","4035288817","yorrick.brown@ucalgary.ca",NULL),
+("3","Clark","Joseph","Kent","4032613398","clark.joseph.kent@ucalgary.ca","Smallville"),
+("4","Tintin",NULL,NULL,"4031571609","tintin@ucalgary.ca","Marlinspike Hall, Belgium"),
+("5","UCalgary",NULL,NULL,"4032811334","ucalgary@ucalgary.ca","2500 University Dr NW, Calgary, AB T2N 1N4"),
+("6","James",NULL,"Gunn","4033203090","james.gunn@ucalgary.ca","9336 Civic Center Drive, Beverly Hills, CA 90210-3604, USA");
 
 UPDATE sequence_owners SET next_val=7;
 -- ----------------------------------------------
 
 
 -- animals --------------------------------------
-INSERT INTO animals (a_animalid,a_name,a_type,a_breed,a_birthdate,a_sex,a_status,a_ownerid,a_tattoonum,a_rfidnumber,a_microchipnumber,a_weight,a_coatcolor,a_continuousmedication) VALUES
-(1,"Ace","Dog","German shepherd",NULL,"1",3,1,130110477,"229664665","229348845",NULL,"Black",NULL),
-(2,"Ampersand","Monkey","Capuchin monkey",NULL,"0",1,2,204261377,"313829102","233453231",NULL,"Brown",NULL),
-(3,"Bat Cow","Cow",NULL,NULL,"0",2,1,171659869,"223023284","202432386",NULL,"Brown, White",NULL),
-(4,"Comet","Horse","Canadian horse",NULL,"1",1,3,225931412,"184668886","345541619",NULL,"White",NULL),
-(5,"Krypto","Dog","Labrador retriever",NULL,"0",1,3,185030356,"170719459","157412204",NULL,"White",NULL),
-(6,"Snowy","Dog","Wire Fox Terrier",NULL,"1",0,4,225864215,"188758885","255400812",NULL,"White",NULL),
-(7,"Streaky","Cat","Abyssinian",NULL,"1",1,3,209577225,"177988155","267795413",NULL,"Orange",NULL);
+INSERT INTO animals (a_animalid,a_name,a_species,a_subspecies,a_breed,a_type,a_region,a_sex,a_birthdate,a_status,a_ownerid,a_profilepic,a_tattoonum,a_citytattoo,a_rfidnumber,a_microchipnumber,a_coatcolor,a_distinctfeature,a_continuousmedication) VALUES
+("1","Ace","Dog","Canine","German shepherd",NULL,"Spain","0","2017-12-01","0","1",NULL,"269617698","HOC sha","266951170","280286676","Black","sleeping pills","he can fly"),
+("2","Ampersand","Monkey","Ape","Capuchin monkey",NULL,"France","1","2018-12-02","3","2",NULL,"235026499","ORE esd","337506275","123933781","Brown",NULL,"high jumps"),
+("3","Bat Cow","Cow","Bovine",NULL,"Dairy","US","1","2020-04-03","0","1",NULL,"181698181","NKN sds","266386520","335668545","Brown, White",NULL,"bat-shaped patch on face"),
+("4","Comet","Horse","Gallopping","Canadian horse",NULL,"Canada","0","2017-12-11","1","3",NULL,"201373110","HIS sdm","127123163","234277856","White",NULL,"spotted"),
+("5","Krypto","Dog","Canine","Labrador retriever",NULL,"Africa","0","2018-12-31","3","3",NULL,"342871357","YSE mlc","324813826","274718671","White",NULL,NULL),
+("6","Snowy","Dog","Canine","Wire Fox Terrier",NULL,"London","1","2020-04-07","2","4",NULL,"182129899","JSD sda","262973735","247482427","White",NULL,"long ears"),
+("7","Streaky","Cat","Feline","Abyssinian",NULL,"Rome","1","2021-04-03","0","3",NULL,"162506178","DLF kjs","199834890","141860062","Orange",NULL,NULL);
 
 UPDATE sequence_animals SET next_val=8;
 -- ----------------------------------------------
 
 
--- users --------------------------------------
-INSERT INTO users (u_userid,u_firstname,u_middlename,u_lastname,u_role,u_emailid,u_password,u_status) VALUES
-(1,"Greg",NULL,"Boorman",0,"greg.boorman@ucalgary.ca","passw0rd",1),
-(2,"Teacher",NULL,"Admin",0,"teacher.admin@ucalgary.ca","passw0rd",0),
-(3,"Technician",NULL,"A",2,"technician.a@ucalgary.ca","passw0rd",0),
-(4,"Attendant",NULL,"B",1,"attendant.b@ucalgary.ca","passw0rd",0),
-(5,"Teacher",NULL,"C",3,"teacher.c@ucalgary.ca","passw0rd",0),
-(6,"Student",NULL,"D",4,"student.d@ucalgary.ca","passw0rd",1),
-(7,"Student",NULL,"E",4,"student.e@ucalgary.ca","passw0rd",1);
+-- users ----------------------------------------
+INSERT INTO users (u_userid,u_joiningdate,u_activationdate,u_terminationdate,u_firstname,u_middlename,u_lastname,u_role,u_emailid,u_passwordhash,u_passwordsalt,u_status) VALUES
+("1","2017-12-01","2017-12-02",NULL,"Greg",NULL,"Boorman","0","greg.boorman@ucalgary.ca","passw0rd",NULL,"1"),
+("2","2018-12-02","2018-12-03",NULL,"Teacher",NULL,"Admin","0","teacher.admin@ucalgary.ca","passw0rd",NULL,"0"),
+("3","2020-04-03","2020-04-04",NULL,"Technician",NULL,"A","2","technician.a@ucalgary.ca","passw0rd",NULL,"0"),
+("4","2017-12-11","2017-12-12",NULL,"Attendant",NULL,"B","1","attendant.b@ucalgary.ca","passw0rd",NULL,"0"),
+("5","2018-12-31","2019-01-01","2021-09-27","Teacher",NULL,"C","3","teacher.c@ucalgary.ca","passw0rd",NULL,"0"),
+("6","2020-04-07","2020-04-08","2023-01-03","Student",NULL,"D","4","student.d@ucalgary.ca","passw0rd",NULL,"0"),
+("7","2021-04-03","2021-04-04","2023-12-30","Student",NULL,"E","4","student.e@ucalgary.ca","passw0rd",NULL,"0");
 
 UPDATE sequence_users SET next_val=8;
 -- ----------------------------------------------
 
 
--- treatments ------------------------------------
-INSERT INTO treatments (t_treatmentid,t_treatmentdesc,t_animalid,t_treatmentdate,t_treatedby) VALUES
-(1,"Physical exam","7","2021-12-08","1"),
-(2,"Blood work","1","2021-12-09","2"),
-(3,"Da2pp","1","2021-12-10","5"),
-(4,"dental cleaning","6","2021-12-08","3"),
-(5,"drontal deworm","1","2021-12-09","4"),
-(6,"rabies vaccination","7","2021-12-10","2"),
-(7,"Revolution treatment","7","2021-12-08","4");
+-- weights --------------------------------------
+INSERT INTO weights (w_weightid,w_massinkg,w_recorddate,w_recordedby,w_animalid) VALUES
+("1","5","2020-10-01","7","1"),
+("2","4","2020-10-01","7","2"),
+("3","30","2020-10-01","1","3"),
+("4","40","2020-10-01","3","4"),
+("5","3","2020-10-01","4","5"),
+("6","3","2020-10-01","4","6"),
+("7","1","2020-10-01","4","7"),
+("8","4","2020-11-03","1","1"),
+("9","5.2","2020-11-03","7","2"),
+("10","4.2","2020-11-03","2","3"),
+("11","30.2","2020-11-03","6","4"),
+("12","40.2","2020-11-03","1","5"),
+("13","3.2","2020-11-03","1","6"),
+("14","3.2","2020-11-03","2","7"),
+("15","0.6","2020-11-29","5","1"),
+("16","3.6","2020-11-29","5","2"),
+("17","4.8","2020-11-29","3","3"),
+("18","3.8","2020-11-29","7","4"),
+("19","29.8","2020-11-29","7","5"),
+("20","39.8","2020-11-29","7","6"),
+("21","2.8","2020-11-29","3","7"),
+("22","3.8","2021-02-28","2","1"),
+("23","1.2","2021-02-28","1","2"),
+("24","4.2","2021-02-28","1","3"),
+("25","5.4","2021-02-28","4","4"),
+("26","4.4","2021-02-28","5","5"),
+("27","30.4","2021-02-28","4","6"),
+("28","40.4","2021-02-28","7","7");
 
-UPDATE sequence_treatments SET next_val=8;
--- ----------------------------------------------
-
-
--- issues ---------------------------------------
-INSERT INTO issues (i_issueid,i_issuedesc,i_animalid,i_detecteddate,i_raisedby,i_isresolved) VALUES
-(1,"Limp Walk","3","2021-12-08","1",1),
-(2,NULL,"6","2020-12-12","1",0),
-(3,"Diabetes","7","2021-05-16","2",0),
-(4,"Inflammed limb","1","2021-05-29","1",1),
-(5,"Bladder Infection","1",NULL,"5",0),
-(6,"chronic kidney disease","7","2021-11-17","3",0),
-(7,"Upset Stomach","3","2021-11-17","2",1);
-
-UPDATE sequence_issues SET next_val=8;
+UPDATE sequence_treatments SET next_val=29;
 -- ----------------------------------------------
 
 
 -- photos ---------------------------------------
-INSERT INTO photos (p_photoid,p_animalid,p_photolink) VALUES
-(1,"7","image1.png"),
-(2,"2","image2.png"),
-(3,"3","image4.png"),
-(4,"2",NULL),
-(5,"3","image7.png"),
-(6,"5","image6.png"),
-(7,"6","image5.png");
+INSERT INTO photos (p_photoid,p_photodesc,p_animalid,p_photolink,p_alttext,p_uploader,p_uploaddate) VALUES
+("1",NULL,"2","image1.png","neck","3","2020-10-01"),
+("2",NULL,"4","image2.png",NULL,"7","2020-10-01"),
+("3",NULL,"1","image4.png",NULL,"6","2020-10-01"),
+("4","leg broken","7",NULL,NULL,"2","2020-10-01"),
+("5",NULL,"2","image7.png",NULL,"6","2020-10-01"),
+("6",NULL,"4","image6.png",NULL,"4","2020-10-01"),
+("7",NULL,"5","image5.png",NULL,"3","2020-10-01");
 
 UPDATE sequence_photos SET next_val=8;
 -- ----------------------------------------------
@@ -83,13 +90,41 @@ UPDATE sequence_photos SET next_val=8;
 
 -- comments -------------------------------------
 INSERT INTO comments (c_commentid,c_commentdesc,c_animalid,c_commentdate,c_commenter) VALUES
-(1,"Nighttime terror","2","2021-12-08","1"),
-(2,"Howling ","2","2021-12-09","4"),
-(3,"Pregnant ","6","2021-12-10","5"),
-(4,"No Comment Provided","1","2021-12-08","5"),
-(5,"No Comment Provided","1","2021-12-09","1"),
-(6,"No Comment Provided","2","2021-12-10","2"),
-(7,"Not sleeping","2","2021-12-08","1");
+("1","Nighttime terror","4","2021-12-08","3"),
+("2","Howling ","6","2021-12-09","5"),
+("3","Pregnant ","2","2021-12-10","3"),
+("4",NULL,"4","2021-12-08","7"),
+("5",NULL,"6","2021-12-09","3"),
+("6",NULL,"6","2021-12-10","7"),
+("7","Not sleeping","1","2021-12-08","6");
 
 UPDATE sequence_comments SET next_val=8;
+-- ----------------------------------------------
+
+
+-- issues ---------------------------------------
+INSERT INTO issues (i_issueid,i_issuedesc,i_detecteddate,i_animalid,i_raisedby,i_isresolved) VALUES
+("1","Limp Walk","2021-12-08","1","1",1),
+("2",NULL,"2020-12-12","1","1",1),
+("3","Diabetes","2021-05-16","6","2",0),
+("4","Inflammed limb","2021-05-29","5","1",0),
+("5","Bladder Infection","2021-05-16","5","5",0),
+("6","chronic kidney disease","2021-11-17","5","3",0),
+("7","Upset Stomach","2021-11-17","4","2",0);
+
+UPDATE sequence_issues SET next_val=8;
+-- ----------------------------------------------
+
+
+-- treatments -----------------------------------
+INSERT INTO treatments (t_treatmentid,t_treatmentdesc,t_drugname,t_drugdose,t_deliverymethod,t_animalid,t_treatmentdate,t_treatedby) VALUES
+("1","Physical exam",NULL,NULL,NULL,"7","2021-12-08","1"),
+("2","Blood work",NULL,NULL,NULL,"7","2021-12-09","2"),
+("3","Da2pp",NULL,NULL,NULL,"4","2021-12-10","5"),
+("4","dental cleaning",NULL,NULL,NULL,"6","2021-12-08","3"),
+("5","drontal deworm",NULL,NULL,NULL,"6","2021-12-09","4"),
+("6","rabies vaccination",NULL,NULL,NULL,"4","2021-12-10","2"),
+("7","Revolution treatment",NULL,NULL,NULL,"5","2021-12-08","4");
+
+UPDATE sequence_treatments SET next_val=8;
 -- ----------------------------------------------

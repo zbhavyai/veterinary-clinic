3e525757fb50cab360b38590302812df4c010f7b
mlee2021 <83717300+mlee2021@users.noreply.github.com>
2021-12-01 09:46:23 -0700

    build: [U888-39] animal profile draws data and plots weight

--- frontend/src/components/animalprofile.jsx
+++ frontend/src/components/animalprofile.jsx
@@ -1,12 +1,105 @@
 import React, { Component } from 'react';
+import axios from "axios";
+import {getAnimalbyId} from '../services/fakeAnimalsService'; 
+import ReactApexChart from 'react-apexcharts';
+
 class AnimalProfile extends React.Component {
     state = {
+        animal: {},
         
         
         imageUrl: 'https://picsum.photos/200',
+
+        
+        
         
     };
+
+    async componentDidMount() {
+        
+        const {data: posts} = await axios.get('http://localhost:8080/api/v1/animals/1', {headers: {'Access-Control-Allow-Origin': true,},});
+        
+        this.setState({posts});
+        
+
+        
+
+        
+            
+        //const promise = axios.get('https://jsonplaceholder.typicode.com/posts')
+        
+    }
+
+    //WARNING! To be deprecated in React v17. Use componentDidMount instead.
+    componentWillMount() {
+        this.setState({animal: getAnimalbyId(1), 
+            
+        });
+
+        
+        
+
+        
+        
+
+    }
+
     render() { 
+        let weight = [];
+        let dates = [];
+        console.log(this.state.animal["weight"]);
+
+        if(this.state.animal["weight"].length>0){
+
+            for(var i = 0; i<this.state.animal["weight"].length; i++){
+                weight.push(this.state.animal["weight"][i]["value"]);
+                dates.push(this.state.animal["weight"][i]["date"]);
+                
+                
+
+            }
+
+            
+            
+           
+
+        }
+        else{
+            weight = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+            dates = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'];
+
+            
+        }
+        const series= [{
+            name: "Weight",
+            data: weight
+        }];
+
+        const options= {
+            chart: {
+              height: 350,
+              type: 'line',
+              zoom: {
+                enabled: false
+              }
+            },
+            dataLabels: {
+              enabled: false
+            },
+            stroke: {
+              curve: 'straight'
+            },
+            
+            grid: {
+              row: {
+                colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+                opacity: 0.5
+              },
+            },
+            xaxis: {categories: dates}
+          }
+
+
         return <React.Fragment>
             <div class="container">
                 <div class="row">
@@ -18,12 +111,12 @@ class AnimalProfile extends React.Component {
                     <div class="jumbotron jumbotron-fluid">
                     <div class="container">
                         <h5 class="display-4">Basic Details</h5>
-                        <p class="lead">Animal ID: </p>
-                        <p class="lead">Name: </p>
-                        <p class="lead">Breed: </p>
-                        <p class="lead">Age: </p>
-                        <p class="lead">Sex: </p>
-                        <p class="lead">Status: </p>
+                        <p class="lead">Animal ID: {this.state.animal["animalId"]}</p>
+                        <p class="lead">Name: {this.state.animal["name"]}</p>
+                        <p class="lead">Breed: {this.state.animal["breed"]}</p>
+                        <p class="lead">Age: {this.state.animal["age"]}</p>
+                        <p class="lead">Sex: {this.state.animal["animalId"]}</p>
+                        <p class="lead">Status: {this.state.animal["sex"]}</p>
                     </div>
                     </div>
                     </div>
@@ -34,10 +127,10 @@ class AnimalProfile extends React.Component {
                         <div class="jumbotron jumbotron-fluid">
                             <div class="container">
                                 <h5 class="display-4">Owner Details</h5>
-                                <p class="lead">Owner Name: </p>
-                                <p class="lead">Owner Contact: </p>
-                                <p class="lead">Owner Email: </p>
-                                <p class="lead">Owner Address: </p>
+                                <p class="lead">Owner Name: {this.state.animal["theOwner"]["firstName"]} {this.state.animal["theOwner"]["lastName"]}</p>
+                                <p class="lead">Owner Contact: {this.state.animal["theOwner"]["contactNumber"]}</p>
+                                <p class="lead">Owner Email: {this.state.animal["theOwner"]["emailId"]}</p>
+                                <p class="lead">Owner Address: {this.state.animal["theOwner"]["address"]}</p>
                                 
                             </div>
                         </div>
@@ -47,10 +140,10 @@ class AnimalProfile extends React.Component {
                         <div class="jumbotron jumbotron-fluid">
                         <div class="container">
                             <h5 class="display-4">Identification Details</h5>
-                            <p class="lead">RFID Number: </p>
-                            <p class="lead">Microchip: </p>
-                            <p class="lead">Tattoo: </p>
-                            <p class="lead">Coat Color: </p>
+                            <p class="lead">RFID Number: {this.state.animal["rfidNumber"]}</p>
+                            <p class="lead">Microchip: {this.state.animal["microChipNumber"]}</p>
+                            <p class="lead">Tattoo: {this.state.animal["tattooNum"]}</p>
+                            <p class="lead">Coat Color: {this.state.animal["coatColor"]}</p>
                             
                         </div>
                         </div>
@@ -61,6 +154,9 @@ class AnimalProfile extends React.Component {
                     <div class="jumbotron jumbotron-fluid">
                         <div class="container">
                             <h5 class="display-4">Weight Graph</h5>
+                            <div id="chart">
+                                <ReactApexChart options={options} series={series} type="line" height={350} />
+                            </div>
                             
                             
                         </div>
@@ -71,7 +167,7 @@ class AnimalProfile extends React.Component {
                     <div class="jumbotron jumbotron-fluid">
                         <div class="container">
                             <h5 class="display-4">Other Details</h5>
-                            <p class="lead">Continuous Medication: </p>
+                            <p class="lead">Continuous Medication: {this.state.animal["continuousMedication"]}</p>
                             
                             
                         </div>
